7674b81bdcaf79e96b73054e16737ffa53eb5e84
leeyonghee030 <leeyonghee030@gmail.com>
2026-08-07 14:18:01 +0900

    도전과제

--- java_v1/src/useful/ch12/Step7.java
+++ java_v1/src/useful/ch12/Step7.java
@@ -1,20 +1,24 @@
 package useful.ch12;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Step7 {
 
     public static void main(String[] args) {
         int sum = 0;
 
-        LocalDateTime b = LocalDateTime.now();
+        LocalTime start = LocalTime.now();
+
         for (int i = 1; i <= 100_000_000; i++) {
             sum += i;
         }
         System.out.println(sum);
-        LocalDateTime c = LocalDateTime.now();
-        Duration work = Duration.between(b, c);
+        LocalTime end = LocalTime.now();
+        Duration work = Duration.between(start, end);
+
+        System.out.println("시작시간 : " + start);
+        System.out.println("종료시간 : " + end);
         System.out.println(work.toMillis() + "ms");
         //29ms = 0.029초
 
